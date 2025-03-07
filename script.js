2f38760e07a8f77f614364e4ee702547c64f177c
JacknS2520 <jackelly0221@gmail.com>
2025-03-07 16:13:09 -0600

    function user(find)

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function user(find) {
+    var usersignedin = "no"
+    var currentusername = "unkown"
+    var userdisplay1 = document.getElementById("account").textContent
+    var signin_button = document.getElementById("signin_button")
+    if(usersignedin == "yes") {
+        userdisplay1 == currentusername;
+        signin_button.style.display = "none";
+        usersignedin = "yes"
+    }
+}
