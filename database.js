f698ac113571893e938876ebd19f1fddba15d159
JacknS2520 <jackelly0221@gmail.com>
2025-03-13 19:25:24 -0500

    Update database.js

--- database.js
+++ database.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'your_username',
-  password: 'your_password',
+  user: 'admin',
+  password: '10thCircle-Admin',
   database: 'your_database'
 });
 
